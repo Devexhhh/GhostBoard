7e78ce089c38b1cb208cd11831d5b0962537cedc
devexxh <kumarsujal.codes@gmail.com>
2026-03-04 22:36:31 +0530

    fixed code indentation

--- ghostboard-client/src/components/ConfessionCard.jsx
+++ ghostboard-client/src/components/ConfessionCard.jsx
@@ -1,30 +1,22 @@
 export default function ConfessionCard({ confession, onVote }) {
     return (
         <div className="card">
-
             <h3>{confession.title}</h3>
             <p>{confession.body}</p>
-
             <div className="actions">
-
                 <button onClick={() => onVote(confession._id, 1)}>
                     ⬆️
                 </button>
-
                 <button onClick={() => onVote(confession._id, -1)}>
                     ⬇️
                 </button>
-
                 <button onClick={() => onVote(confession._id, 0)}>
                     ❌
                 </button>
-
                 <span className="score">
                     Score: {confession.score}
                 </span>
-
             </div>
-
         </div>
     );
 }
